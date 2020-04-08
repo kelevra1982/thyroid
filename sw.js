540947bd3c80e616c0b7039366c79ef1c7e399f6
Gunther Lehmann <g.lehmann@blitzen.de>
2020-04-08 12:46:33 +0200

    add icons to lists, remove splash screen

--- sw.js
+++ sw.js
@@ -7,7 +7,6 @@ self.addEventListener('install', function(e)
 			'/',
 			'/index.html',
 			'/manifest.json',
-			'/img/header.jpg',
 			'https://unpkg.com/onsenui/css/onsenui.min.css',
 			'https://unpkg.com/onsenui/css/onsen-css-components.min.css',
 			'https://fonts.googleapis.com/css2?family=Oleo+Script:wght@400;700&display=swap',
@@ -15,7 +14,15 @@ self.addEventListener('install', function(e)
 			'https://unpkg.com/onsenui/js/onsenui.min.js',
 			'https://unpkg.com/jquery/dist/jquery.min.js',
 			'https://cdnjs.cloudflare.com/ajax/libs/inobounce/0.2.0/inobounce.js',
-			'/js/thyroid.js'
+			'/js/thyroid.js',
+			'/img/header.jpg',
+			'/img/ft3.png',
+			'/img/ft4.png',
+			'/img/tsh.png',
+			'/img/vitamind.png',
+			'/img/beschwerden.png',
+			'/img/medikation.png',
+			'/img/statistik.png'
 		]);
 	}));
 });

4ddaede3a200d18423c2eb930fafefdb9b3daab2
Gunther Lehmann <g.lehmann@blitzen.de>
2020-04-08 13:10:07 +0200

    changed icons

---
+++ api/config.php
@@ -0,0 +1,13 @@
+<?php
+
+	/*define('HOST', 'localhost');
+	define('USER', 'root');
+	define('PASSWORD', '');
+	define('DB', 'my-thyroid');*/
+
+	define('HOST', 'localhost');
+	define('USER', 'd031def3');
+	define('PASSWORD', 'L7ant9QRBZW4bKzM');
+	define('DB', 'd031def3');
+
+?>

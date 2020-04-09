5a8c6bc8bf795226b065553e8cf5885d201efd3b
Gunther Lehmann <g.lehmann@blitzen.de>
2020-04-09 11:35:04 +0200

    add font icons to cache

--- sw.js
+++ sw.js
@@ -9,6 +9,7 @@ self.addEventListener('install', function(e)
 			'/manifest.json',
 			'https://unpkg.com/onsenui/css/onsenui.min.css',
 			'https://unpkg.com/onsenui/css/onsen-css-components.min.css',
+			'https://unpkg.com/onsenui/css/material-design-iconic-font/css/material-design-iconic-font.min.css',
 			'https://fonts.googleapis.com/css2?family=Oleo+Script:wght@400;700&display=swap',
 			'/css/thyroid.css',
 			'https://unpkg.com/onsenui/js/onsenui.min.js',
@@ -36,4 +37,4 @@ self.addEventListener('fetch', function(e)
 	e.respondWith(caches.match(e.request).then(response => response || fetch(e.request)));
 });
 
-// last Update: 08.04.2020 10:28
+// last Update: 09.04.2020 11:34

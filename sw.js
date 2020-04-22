d855f647373611ee93c9c5204f5714626a953625
Gunther Lehmann <g.lehmann@blitzen.de>
2020-04-22 09:02:35 +0200

    Update sw.js
    update time sw.js

--- sw.js
+++ sw.js
@@ -53,4 +53,4 @@ self.addEventListener('fetch', function(e)
 	e.respondWith(caches.match(e.request).then(response => response || fetch(e.request)));
 });
 
-// last Update: 22.04.2020 08:53
+// last Update: 22.04.2020 09:02
